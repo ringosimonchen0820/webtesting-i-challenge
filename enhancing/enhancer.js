c141f92c281da7ee4650d9e78e9471f488f5373a
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-04-23 16:21:50 -0400

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,44 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
-}
+  if(item.enhancement < 20) {
+    item.enhancement += 1;
+    return item;
+  } else {
+    return item;
+  }
+};
 
+// function fail(item) {
+//   if (item.enhancement >= 0 && item.enhancement < 15) {
+//     item.durability -= 5;
+//     return item;
+//   } if (item.enhancement >= 15) {
+//     item.durability -= 10;
+//     return item;
+//   } if (item.enhancement > 16) {
+//     item.enhancement -= 1;
+//     return item;
+//   } 
+// };
 function fail(item) {
-  return { ...item };
-}
+  if (item.enhancement >= 0 && item.enhancement < 15) {
+    item.durability -= 5;
+    return item;
+  } if (item.enhancement >= 15) {
+    item.durability -= 10;
+    if (item.enhancement > 16) {
+      item.enhancement -= 1;
+      return item;
+    } else {
+      return item;
+    }
+  }
+};
 
 function repair(item) {
-  return { ...item };
+  item.durability = 100;
+  return item;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,47 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require('./enhancer.js');
 // test away!
+it('should run the tests', function() {
+  expect(true).toBe(true);
+});
+
+describe('enhancer.js', function() {
+  describe('.succeed()', function() {
+    it('should increase enhancement by 1 if under 20', function() {
+      expect(succeed({ enhancement: 15 })).toEqual({ enhancement: 16 });
+      expect(succeed({ enhancement: 10 })).toEqual({ enhancement: 11 });
+      expect(succeed({ enhancement: 1 })).toEqual({ enhancement: 2 });
+      expect(succeed({ enhancement: 0 })).toEqual({ enhancement: 1 });
+      expect(succeed({ enhancement: 19 })).toEqual({ enhancement: 20 });
+    })
+
+    it('should keep enhancement the same if it is 20', function() {
+      expect(succeed({ enhancement: 20 })).toEqual({ enhancement: 20 });
+    });
+  });
+
+  describe('.fail()', function() {
+    it('should decrease durability by 5 if enhancement is less than 15', function() {
+      expect(fail({ enhancement: 14, durability: 30 })).toEqual({ enhancement: 14, durability: 25 });
+    })
+
+    it('should decrease durability by 10 if enhancement is 15 or more', function() {
+      expect(fail({ enhancement: 15, durability: 60 })).toEqual({ enhancement: 15, durability: 50 });
+      expect(fail({ enhancement: 16, durability: 69 })).toEqual({ enhancement: 16, durability: 59 });
+    })
+
+    it('should decrease enhancement by 1 if enhancement is 16 or more', function() {
+      expect(fail({ enhancement: 18, durability: 100 })).toEqual({ enhancement: 17, durability: 90 });
+      expect(fail({ enhancement: 19, durability: 69 })).toEqual({ enhancement: 18, durability: 59 });
+      expect(fail({ enhancement: 17, durability: 20 })).toEqual({ enhancement: 16, durability: 10 });
+    })
+  })
+  
+  describe('.repair()', function() {
+    it('should return a new item with durability of 100', function() {
+      expect(repair({ durability: 10 })).toEqual({ durability: 100 });
+      expect(repair({ durability: 99 })).toEqual({ durability: 100 });
+      expect(repair({ durability: 0 })).toEqual({ durability: 100 });
+    });
+  });
+
+});
